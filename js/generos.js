7fef0c878640c7bdac9e070b68ef31012a59e5d6
Cristhel <cristhelguarda24@gmail.com>
2022-12-20 09:17:42 -0600

    Se agrega el Proyecto

---
+++ js/generos.js
@@ -0,0 +1,106 @@
+const preguntas = [
+    {
+        pregunta: "¿Cuál es el nombre del genero musical de corea del sur?",
+        respuestas: ["reggeton ", "trap", "kpop", "pop"],
+        correcta: 2
+    },
+    {
+        pregunta: "¿Cuál es el genero mas escuchado?",
+        respuestas: ["kpop", "bachata", "merengue", "reggeton"],
+        correcta: 3
+    },
+    {
+        pregunta: "¿Cual es el genero de la canción imagine de john lennon ?",
+        respuestas: ["soft rok ", "trap", "reggeton", "rap"],
+        correcta: 0
+    }
+];
+
+let indice_aleatorio = 0;
+
+let pregunta_txt = "";
+
+let interval;
+
+window.onload = iniciar();
+
+function iniciar() {
+    loadQuestions();
+    if (localStorage.getItem("SCORE") != null) {
+        localStorage.removeItem("SCORE");
+    }
+    }
+
+
+function iniciarCronometro() {
+  const contador = 10, cronometroDisplay = document.getElementById("cronometro")
+
+  iniciarTiempo(contador, cronometroDisplay)
+  
+}
+
+function iniciarTiempo(duracion, componente) {
+    interval = setInterval(() => {
+    if (duracion === 0) {
+
+      componente.innerHTML = "Se acabó el tiempo";
+
+      clearInterval(interval);
+
+      loadQuestions()
+
+    } else {
+      
+      duracion = duracion < 10 ? "0" + duracion : duracion;
+
+      componente.textContent = "00:" + duracion;
+
+      duracion--;
+    }
+    }, 1000)
+
+}
+
+function loadQuestions() {
+  iniciarCronometro()
+    if (preguntas.length > 0) {
+
+        indice_aleatorio = Math.floor(Math.random() * preguntas.length);
+
+        pregunta_txt = "";
+
+        pregunta_txt += '<p class="pregunta">' + preguntas[indice_aleatorio].pregunta + '</p>';
+
+        pregunta_txt += '<button id="opcion0" class="botonTrivias" onclick="verificarRespuestaCorrecta(0, ' + preguntas[indice_aleatorio].correcta + ')">' + preguntas[indice_aleatorio].respuestas[0] + '</button>';
+
+        pregunta_txt += '<button id="opcion1" class="botonTrivias" onclick="verificarRespuestaCorrecta(1, ' + preguntas[indice_aleatorio].correcta + ')">' + preguntas[indice_aleatorio].respuestas[1] + '</button>';
+
+        pregunta_txt += '<button id="opcion2" class="botonTrivias" onclick="verificarRespuestaCorrecta(2, ' + preguntas[indice_aleatorio].correcta + ')">' + preguntas[indice_aleatorio].respuestas[2] + '</button>';
+
+        pregunta_txt += '<button id="opcion3" class="botonTrivias" onclick="verificarRespuestaCorrecta(3, ' + preguntas[indice_aleatorio].correcta + ')">' + preguntas[indice_aleatorio].respuestas[3] + '</button>';
+
+        document.getElementById("pregunta").innerHTML = pregunta_txt;
+
+        preguntas.splice(indice_aleatorio, 1);
+
+    } else {
+        window.location.href = "../html/puntaje.html";
+    }
+}
+
+let puntos = 0;
+
+function verificarRespuestaCorrecta(indice, correcta) {
+    if (correcta === indice) {
+        puntos = puntos + 5;   
+    }
+    
+    localStorage.setItem("SCORE", puntos);
+
+    document.getElementById("opcion0").disabled = true;
+    document.getElementById("opcion1").disabled = true;
+    document.getElementById("opcion2").disabled = true;
+    document.getElementById("opcion3").disabled = true;
+}
+
+document.getElementById("siguienteTrivia").addEventListener("click", () => { clearInterval(interval), loadQuestions() });
---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", guardarNombre);
+
+function guardarNombre() {
+  let  nombreUsuario = document.getElementById("nombre").value;
+    localStorage.setItem("name", nombreUsuario);
+
+}
